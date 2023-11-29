ce4623e94d15267fe42a22ed647276542411e9e0
Solomon Udofia <meetsolomonudofia@gmail.com>
2023-11-30 00:20:12 +0100

    Created the nav bar and add some styling

--- ecom/src/App.tsx
+++ ecom/src/App.tsx
@@ -1,12 +1,18 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav";
 
 function App() {
-
   return (
-    <>
-        
-    </>
-  )
+    <section>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route />
+        </Routes>
+      </BrowserRouter>
+    </section>
+  );
 }
 
-export default App
+export default App;
---
+++ ecom/src/components/Nav.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <section className="common-settings nav-section">
+      <div className="nav">
+        <p className="nav-logo">Logo</p>
+        <div className="nav-input-div">
+          <input className="nav-input" placeholder="Search products" />
+          <button className="nav-search-button" type="submit">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="#808080"
+              className="bi bi-search"
+              viewBox="0 0 16 16"
+            >
+              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+            </svg>
+          </button>
+        </div>
+        <div className="nav-cart-icon-div">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            fill="#808080"
+            className="bi bi-cart3"
+            viewBox="0 0 16 16"
+          >
+            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
+          </svg>
+          <span className="cart-quantity">0</span>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Nav;
