73dc2da8f7b2684bdf118e0f0a3ec5166966846f
Solomon Udofia <meetsolomonudofia@gmail.com>
2024-01-07 22:17:20 +0100

    updated EmptyCart.tsx

--- src/components/sub/EmptyCart.tsx
+++ src/components/sub/EmptyCart.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import emptyCart from '../../asset/emptyCart.jpgq'
+import emptyCart from '../../asset/emptyCart.jpg'
 
 const EmptyCart = () => {
   return (
